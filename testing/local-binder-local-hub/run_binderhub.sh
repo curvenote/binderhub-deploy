1f64e2bf1006cbcb9025aaf31cb6589d49ae4691
Steve Purves <steve@curvenote.com>
2023-07-11 18:17:24 +0100

    echo and a little log rotation

--- testing/local-binder-local-hub/run_binderhub.sh
+++ testing/local-binder-local-hub/run_binderhub.sh
@@ -1,2 +1,3 @@
 source ./configure.sh
+echo "Starting Binderhub in Terminal - terminal must stay open"
 jupyterhub --config=jupyterhub_config.py &
--- testing/local-binder-local-hub/run_binderhub_nohup.sh
+++ testing/local-binder-local-hub/run_binderhub_nohup.sh
@@ -1,2 +1,4 @@
 source ./configure.sh
+echo "Starting Binderhub in Background - this terminal can be closed"
+mv nohup.out nohup.out.old
 nohup jupyterhub --config=jupyterhub_config.py &
